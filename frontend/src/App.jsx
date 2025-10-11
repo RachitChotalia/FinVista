7b542f29f9f7cc589b7223592fc84185a6ff7e1f
RachitChotalia <chotaliarachit@gmail.com>
2025-10-11 19:23:30 +0530

    fixed import issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage.jsx';
 import RegisterPage from './pages/RegisterPage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+
 
 function App() {
   return (
